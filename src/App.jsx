981d44990b918529f09a502d07a451f48467b0f5
Pritam Saha <saha7pritam@gmail.com>
2024-12-07 01:17:04 +0530

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="text-center font-bold text-xl">LinkedIn Connect All</h1>
+      <p className="text-center mt-4">
+        This Chrome extension automates LinkedIn connection requests.
+      </p>
+    </div>
+  );
+}
+
+export default App;
